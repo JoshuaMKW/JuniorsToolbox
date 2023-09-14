78374678c6c09bfd654574c45f60cab87ea6d670
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2023-09-14 01:53:43 -0500

    Skeleton implement some loading code.

--- include/objlib/template.hpp
+++ include/objlib/template.hpp
@@ -26,9 +26,8 @@ namespace Toolbox::Object {
     class Template : public ISerializable {
     public:
         Template() = delete;
-        Template(std::string_view type, std::string_view name)
-            : m_type(type), m_name(name) {}
-        Template(std::string_view type, Deserializer &in) : m_type(type) { deserialize(in); } 
+        Template(std::string_view type, std::string_view name) : m_type(type), m_name(name) {}
+        Template(std::string_view type, Deserializer &in) : m_type(type) { deserialize(in); }
         Template(const Template &) = default;
         Template(Template &&)      = default;
         ~Template()                = default;
@@ -45,12 +44,31 @@ namespace Toolbox::Object {
         std::expected<void, SerialError> deserialize(Deserializer &in) override;
 
     private:
+        void cacheEnums(json &enums);
+        void cacheStructs(json &structs);
+
+        std::optional<MetaMember>
+        loadMemberEnum(std::string_view name, size_t array_size);
+
+        std::optional<MetaStruct>
+        loadMemberStruct(std::string_view name, size_t array_size);
+
+        std::optional<MetaMember> loadMemberPrimitive(std::string_view name, std::string_view type,
+                                                      size_t array_size);
+
+        void loadMembers(json &members, json &structs, json &enums);
+        void loadWizards(json &wizards);
+
+    private:
         std::string m_type;
         std::string m_name;
-        std::vector<MetaEnum> m_enums         = {};
-        std::vector<MetaStruct> m_structs     = {};
+
         std::vector<MetaMember> m_members     = {};
         std::vector<TemplateWizard> m_wizards = {};
+
+        
+        std::vector<MetaStruct> m_struct_cache = {};
+        std::vector<MetaEnum> m_enum_cache    = {};
     };
 
 }  // namespace Toolbox::Object
--- src/objlib/template.cpp
+++ src/objlib/template.cpp
@@ -55,49 +55,8 @@ namespace Toolbox::Object {
             m_name = j.at(0).get<std::string>();
 
             json metadata = j[m_name];
-            {
-                json enums = metadata["Enums"];
-                for (const auto &item : enums.items()) {
-                    const auto &e = item.value();
-                    auto enum_type = magic_enum::enum_cast<MetaType>(e["type"].get<std::string>());
-                    if (!enum_type.has_value()) {
-                        continue;
-                    }
-                    bool enum_bitmask = e["Multi"].get<bool>();
-                    std::vector<MetaEnum::enum_type> values;
-                    for (auto &value : e["Flags"].items()) {
-                        auto vs = value.value().get<std::string>();
-                        MetaEnum::enum_type enumv = {value.key(),
-                                                     MetaValue(std::stoi(vs, nullptr, 0))};
-                        values.push_back(enumv);
-                    }
-                    MetaEnum menum(item.key(), enum_type.value(), values, enum_bitmask);
-                    m_enums.emplace_back(e);
-                }
-            }
-            {
-                json structs = metadata["Structs"];
-                for (const auto &s : structs) {
-                    m_structs.emplace_back(s);
-                }
-            }
-            {
-                json members = metadata["Members"];
-                for (const auto &m : members) {
-                    m_members.emplace_back(m);
-                }
-            }
-            {
-                json wizards = metadata["Wizard"];
-                for (const auto &w : wizards) {
-                    TemplateWizard wizard;
-                    wizard.m_name = w["name"].get<std::string>();
-                    for (const auto &m : w["members"]) {
-                        wizard.m_init_members.emplace_back(m);
-                    }
-                    m_wizards.emplace_back(wizard);
-                }
-            }
+            loadMembers(metadata["Members"], metadata["Structs"], metadata["Enums"]);
+            loadWizards(metadata["Wizard"]);
         });
 
         if (!result) {
@@ -107,6 +66,62 @@ namespace Toolbox::Object {
         }
 
         return {};
+    }
+
+    void Template::cacheEnums(json &enums) {
+        for (const auto &item : enums.items()) {
+            const auto &e  = item.value();
+            auto enum_type = magic_enum::enum_cast<MetaType>(e["type"].get<std::string>());
+            if (!enum_type.has_value()) {
+                continue;
+            }
+            bool enum_bitmask = e["Multi"].get<bool>();
+            std::vector<MetaEnum::enum_type> values;
+            for (auto &value : e["Flags"].items()) {
+                auto vs                   = value.value().get<std::string>();
+                MetaEnum::enum_type enumv = {value.key(), MetaValue(std::stoi(vs, nullptr, 0))};
+                values.push_back(enumv);
+            }
+            MetaEnum menum(item.key(), enum_type.value(), values, enum_bitmask);
+            m_enum_cache.emplace_back(e);
+        }
+    }
+
+    void Template::cacheStructs(json &structs) {}
+
+    std::optional<MetaMember> Template::loadMemberEnum(std::string_view name, size_t array_size) {
+        return std::optional<MetaMember>();
+    }
+
+    std::optional<MetaStruct> Template::loadMemberStruct(std::string_view name, size_t array_size) {
+        return std::optional<MetaStruct>();
+    }
+
+    std::optional<MetaMember>
+    Template::loadMemberPrimitive(std::string_view name, std::string_view type, size_t array_size) {
+        auto vtype = magic_enum::enum_cast<MetaType>(type);
+        if (!vtype)
+            return {};
+
+        std::vector<MetaValue> values;
+        values.reserve(array_size);
+        for (size_t i = 0; i < array_size; ++i) {
+        }
+        return MetaMember(name, values);
+    }
+
+    void Template::loadMembers(json &members, json &structs, json &enums) {}
+
+    void Template::loadWizards(json &wizards) {
+        for (const auto &item : wizards.items()) {
+            TemplateWizard wizard;
+            wizard.m_name = item.key();
+            for (const auto &mi : item.value()["members"].items()) {
+                /* TODO: Properly initialize these according to members */
+                wizard.m_init_members.emplace_back(MetaMember(mi.key(), MetaValue(0)));
+            }
+            m_wizards.emplace_back(wizard);
+        }
     }
 
 }  // namespace Toolbox::Object
