c656df92a0ccd24a9067823c1a46e78f896e7b57
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2023-09-15 03:27:45 -0500

    Comment on main testing

--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,11 @@ int main(int argc, char **argv) {
     test_enum.setFlag("PANTS", false);
 
 
-    test_enum.dump(std::cout, 2, -1);
+    // Demonstrate enum info dumping with an indention of 2.
+    test_enum.dump(std::cout, 2);
+    std::cout << std::endl;
 
+    // Demonstrate the shared resources of Meta types
     MetaStruct test_struct_npc_info("NPCInfo", {MetaMember("mName", MetaValue(std::string("John"))),
                                                 MetaMember("mNoseSize", MetaValue(9000.1))});
     MetaStruct test_struct_npc_info_copy = test_struct_npc_info;
@@ -34,6 +37,7 @@ int main(int argc, char **argv) {
         std::cout << "Member not found" << std::endl;
     }
 
+    // Demonstrate the deep copy of Meta types
     auto deep_copy = std::reinterpret_pointer_cast<MetaStruct, Toolbox::IClonable>(test_struct_npc_info_copy.clone(true));
     if (deep_copy) {
         auto name_member = deep_copy->getMember("mName");
@@ -52,29 +56,33 @@ int main(int argc, char **argv) {
         "NPC", {MetaMember("mHeight", MetaValue(6.2)), MetaMember("mClothes", test_enum),
                 MetaMember("mInfo", {test_struct_npc_info, test_struct_npc_info_copy, *deep_copy})});
 
-    test_struct.dump(std::cout, 0, 1, false);
 
+    // Demonstrate structural info dumping
+    test_struct.dump(std::cout);
+
+    // Demonstrate structural searches by printing a found member
     auto clothes = test_struct.getMember("mClothes");
     if (clothes) {
         if (std::holds_alternative<std::shared_ptr<MetaStruct>>(clothes.value())) {
             auto &struct_clothes = std::get<std::shared_ptr<MetaStruct>>(clothes.value());
-            struct_clothes->dump(std::cout, 0, 1, false);
+            struct_clothes->dump(std::cout);
         } else {
             auto &member_clothes = std::get<std::shared_ptr<MetaMember>>(clothes.value());
-            member_clothes->dump(std::cout, 0, 1);
+            member_clothes->dump(std::cout);
         }
     } else {
         std::cout << "Member not found" << std::endl;
     }
 
+    // Demonstrate exotic structural searches by printing a found scoped member at an array index
     auto info = test_struct.getMember(QualifiedName({"mInfo[1]", "mName"}));
     if (info) {
         if (std::holds_alternative<std::shared_ptr<MetaStruct>>(info.value())) {
             auto &struct_info = std::get<std::shared_ptr<MetaStruct>>(info.value());
-            struct_info->dump(std::cout, 0, 1, false);
+            struct_info->dump(std::cout);
         } else {
             auto &member_info = std::get<std::shared_ptr<MetaMember>>(info.value());
-            member_info->dump(std::cout, 0, 1);
+            member_info->dump(std::cout);
         }
     } else {
         std::cout << "Member not found" << std::endl;
