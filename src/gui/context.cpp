fb885d080d45d2262dedc9e648a0f983ec3052a9
Astral-C <astral-c@users.noreply.github.com>
2023-10-13 14:43:39 -0400

    replace icu with iconv temporarily

--- src/gui/context.cpp
+++ src/gui/context.cpp
@@ -13,9 +13,8 @@
 #include <gui/IconsForkAwesome.h>
 #include <gui/modelcache.hpp>
 
-#include <unicode/unistr.h>
-#include <unicode/ustring.h>
-#include <unicode/ucnv.h>
+
+#include <iconv.h>
 
 namespace Toolbox::UI {
 
@@ -32,6 +31,14 @@ void PacketSort(J3DRendering::SortFunctionArgs packets) {
         }
     );
 }
+
+/* icu 
+
+includes:
+
+#include <unicode/unistr.h>
+#include <unicode/ustring.h>
+#include <unicode/ucnv.h>
 
 std::string Utf8ToSjis(const std::string& value)
 {
@@ -53,6 +60,51 @@ std::string SjisToUtf8(const std::string& value)
     src.extract(0, src.length(), &result[0], "utf8");
 
     return std::string(result.begin(), result.end() - 1);
+}
+*/
+
+std::string Utf8ToSjis(const std::string& value) {
+    iconv_t conv = iconv_open("SHIFT_JISX0213", "UTF-8");
+    if (conv == (iconv_t)(-1)) {
+        throw std::runtime_error("Error opening iconv for UTF-8 to Shift-JIS conversion");
+    }
+
+    size_t inbytesleft = value.size();
+    char* inbuf = const_cast<char*>(value.data());
+
+    size_t outbytesleft = value.size() * 2;
+    std::string sjis(outbytesleft, '\0');
+    char* outbuf = &sjis[0];
+
+    if (iconv(conv, &inbuf, &inbytesleft, &outbuf, &outbytesleft) == (size_t)(-1)) {
+        throw std::runtime_error("Error converting from UTF-8 to Shift-JIS");
+    }
+
+    sjis.resize(sjis.size() - outbytesleft);
+    iconv_close(conv);
+    return sjis;
+}
+
+std::string SjisToUtf8(const std::string& value) {
+    iconv_t conv = iconv_open("UTF-8", "SHIFT_JISX0213");
+    if (conv == (iconv_t)(-1)) {
+        throw std::runtime_error("Error opening iconv for Shift-JIS to UTF-8 conversion");
+    }
+
+    size_t inbytesleft = value.size();
+    char* inbuf = const_cast<char*>(value.data());
+
+    size_t outbytesleft = value.size() * 3;
+    std::string utf8(outbytesleft, '\0');
+    char* outbuf = &utf8[0];
+
+    if (iconv(conv, &inbuf, &inbytesleft, &outbuf, &outbytesleft) == (size_t)(-1)) {
+        throw std::runtime_error("Error converting from Shift-JIS to UTF-8");
+    }
+
+    utf8.resize(utf8.size() - outbytesleft);
+    iconv_close(conv);
+    return utf8;
 }
 
 void DrawTree(std::shared_ptr<Toolbox::Object::ISceneObject> root){
