b08964ce1063552059d7ef572c729bc57db77418
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2024-01-03 01:52:18 -0600

    Fix some stuff

--- include/gui/context_menu.hpp
+++ include/gui/context_menu.hpp
@@ -21,8 +21,7 @@ namespace Toolbox::UI {
 
     template <typename _DataT> class ContextMenu {
     public:
-        using result_t   = std::expected<void, BaseError>;
-        using operator_t = std::function<result_t(_DataT)>;
+        using operator_t = std::function<void(_DataT)>;
 
         struct ContextOp {
             std::string m_name;
@@ -38,10 +37,10 @@ namespace Toolbox::UI {
         void addOption(std::string_view label, std::initializer_list<int> keybind, operator_t op);
         void addDivider();
 
-        result_t render(std::optional<std::string> label, _DataT ctx);
+        void render(std::optional<std::string> label, _DataT ctx);
 
     protected:
-        result_t processKeybinds(_DataT ctx);
+        void processKeybinds(_DataT ctx);
 
     private:
         std::vector<ContextOp> m_options;
@@ -67,12 +66,11 @@ namespace Toolbox::UI {
     }
 
     template <typename _DataT>
-    inline ContextMenu<_DataT>::result_t
-    ContextMenu<_DataT>::render(std::optional<std::string> label, _DataT ctx) {
+    inline void ContextMenu<_DataT>::render(std::optional<std::string> label, _DataT ctx) {
         processKeybinds(ctx);
 
         if (!ImGui::BeginPopupContextItem(label ? label->c_str() : nullptr))
-            return {};
+            return;
 
         for (size_t i = 0; i < m_options.size(); ++i) {
             ContextOp &option = m_options.at(i);
@@ -83,17 +81,7 @@ namespace Toolbox::UI {
             std::string keybind_name = "";
             for (size_t j = 0; j < option.m_keybind.size(); ++j) {
                 int key = option.m_keybind.at(j);
-                if ((key & ImGuiMod_Alt)) {
-                    keybind_name += "Alt";
-                } else if ((key & ImGuiMod_Ctrl)) {
-                    keybind_name += "Ctrl";
-                } else if ((key & ImGuiMod_Shift)) {
-                    keybind_name += "Shift";
-                } else if ((key & ImGuiMod_Super)) {
-                    keybind_name += "Super";
-                } else {
-                    keybind_name += keyNameFromEnum(key);
-                }
+                keybind_name += keyNameFromEnum(key);
                 if (j < option.m_keybind.size() - 1) {
                     keybind_name += "+";
                 }
@@ -104,20 +92,15 @@ namespace Toolbox::UI {
                                            : std::format("{} ({})", option.m_name, keybind_name);
 
             if (ImGui::MenuItem(display_name.c_str())) {
-                auto result = option.m_op(ctx);
-                if (!result) {
-                    ImGui::EndPopup();
-                    return std::unexpected(result.error());
-                }
+                option.m_op(ctx);
             }
         }
 
         ImGui::EndPopup();
-        return {};
+        return;
     }
 
-    template <typename _DataT>
-    inline ContextMenu<_DataT>::result_t ContextMenu<_DataT>::processKeybinds(_DataT ctx) {
+    template <typename _DataT> inline void ContextMenu<_DataT>::processKeybinds(_DataT ctx) {
         ImGuiIO &io = ImGui::GetIO();
         for (size_t i = 0; i < m_options.size(); ++i) {
             ContextOp &option = m_options.at(i);
@@ -127,14 +110,14 @@ namespace Toolbox::UI {
 
             if (keybind_pressed && !option.m_keybind_used) {
                 option.m_keybind_used = true;
-                return option.m_op(ctx);
+                option.m_op(ctx);
             }
 
             if (!keybind_pressed) {
                 option.m_keybind_used = false;
             }
         }
-        return {};
+        return;
     }
 
 }  // namespace Toolbox::UI
