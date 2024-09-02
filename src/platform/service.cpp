6e3cf230b877554b4256c1be2d5483f23b99a27c
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2024-09-02 15:18:09 -0700

    Only define IsServiceRunning on Windows
    I think this is a windows specific api, the compiler doesn't seem to
    mind it being gone on linux

--- src/platform/service.cpp
+++ src/platform/service.cpp
@@ -7,6 +7,7 @@
 
 namespace Toolbox::Platform {
 
+#ifdef TOOLBOX_PLATFORM_WINDOWS
     Result<bool, BaseError> IsServiceRunning(std::string_view name) {
         // Open the Service Control Manager
         SC_HANDLE scm_handle = OpenSCManager(nullptr, nullptr, SC_MANAGER_ENUMERATE_SERVICE);
@@ -44,5 +45,6 @@ namespace Toolbox::Platform {
 
         return isRunning;
     }
+#endif
 
 }  // namespace Toolbox::Platform
